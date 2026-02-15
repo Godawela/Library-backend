74690c3cbbf564edd9bcb703ef1c5146a8cd11a5
Godawela <godawelaus.21@uom.lk>
2026-02-15 18:32:51 +0530

    Fix code issues with Sonarqube

--- src/main/java/com/gl/lms/dto/AuthorsDTO.java
+++ src/main/java/com/gl/lms/dto/AuthorsDTO.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
